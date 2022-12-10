b56cb918df78df371d0b76466321a7534e04d4e0
Matias <matiasnfigueroa98@gmail.com>
2022-12-10 00:57:08 -0300

    modelsv2

---
+++ src/Model/Obras/Comercio.java
@@ -0,0 +1,16 @@
+
+package Model.Obras;
+
+import Model.Empleados.Empleado;
+import java.util.ArrayList;
+
+
+public class Comercio extends ObrasComerciales {
+    private String rubro;
+    
+    
+    
+    
+    
+    
+}
--- src/Model/Obras/ObrasComerciales.java
+++ src/Model/Obras/ObrasComerciales.java
@@ -1,7 +1,29 @@
 
 package Model.Obras;
 
+import Model.Empleados.Empleado;
+import java.util.ArrayList;
 
-public class ObrasComerciales extends Obra {
+
+public abstract class ObrasComerciales extends Obra {
+    protected String nombreObra;
+
+    public ObrasComerciales(String nombreObra, String direccion, double cantidadm2, double tiempoObra, double costom2, ArrayList<Empleado> listaPersonal) {
+        super(direccion, cantidadm2, tiempoObra, costom2, listaPersonal);
+        this.nombreObra = nombreObra;
+    }
+
+    public String getNombreObra() {
+        return nombreObra;
+    }
+
+    public void setNombreObra(String nombreObra) {
+        this.nombreObra = nombreObra;
+    }
+    
+    
+    
+    
+    
     
 }
